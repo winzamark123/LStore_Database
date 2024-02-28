5ff7f3918c1931763afa8ac0058c307dc2b9764a
Nikko Sanchez <n.sanchez7580@gmail.com>
2024-02-27 23:46:27 -0800

    push new physical page

--- lstore/physical_page.py
+++ lstore/physical_page.py
@@ -1,38 +1,49 @@
-from lstore.config import *
+import lstore.config as Config
 import os
+
 class Physical_Page:
     """
     :param entry_size: int          # size of entry in bytes for this page (column)
     :param column_number: int       # number of this page (column) in the base page or tail page
     :param page_number: int         # page_number of this physical page corresponds to the base page it's on
-    
-    """
-    def __init__(self, entry_size:int, column_number:int):
-        self.num_records = 0
-        # entry size of physical page entry differs between columns (RID colum: 2 bytes, StudentID column: 8 bytes, etc..)
-        self.entry_size = entry_size 
-        self.column_number = column_number 
-        self.updates = 0
-        if(column_number == 0):
-            self.data = bytearray(INDIRECTION_PAGE_SIZE) # Indirection column is smaller
-        else:
-            self.data = bytearray(PHYSICAL_PAGE_SIZE)
 
-    # checks capacity of page
-    def has_capacity(self)->bool:
-        if(self.column_number == 0):
-            return ((self.num_records + 1) * self.entry_size <= INDIRECTION_PAGE_SIZE) and ((self.num_records + 1) <= RECORDS_PER_PAGE)
-        else:
-            return ((self.num_records + 1) * self.entry_size <= PHYSICAL_PAGE_SIZE) and ((self.num_records + 1) <= RECORDS_PER_PAGE)
+    """
+    def __init__(self, entry_size:int, column_index:int):
+        # entry size of physical page entry differs between columns (RID column: 2 bytes, StudentID column: 8 bytes, etc..)
+        self.num_records = 0
+        self.entry_size = entry_size
+        self.column_index = column_index
+        self.updates = 0
+        self.page_size = Config.INDIRECTION_PAGE_SIZE if column_index == 0 else Config.PHYSICAL_PAGE_SIZE
+        self.data = bytearray(self.page_size)
+    
+    def __has_capacity(self)->bool:
+        """
+        # checks capacity of page
+        """
+
+        return ((self.num_records + 1) * self.entry_size) <= self.page_size and ((self.num_records + 1) <= Config.RECORDS_PER_PAGE)
+
+    def __get_offset(self, rid:int)->int:
+        """
+        Calculates our offset to know where the RID entry is at in the physical page
+        """
+
+        if(rid < 0):
+            rid = abs(rid)
+        return (rid - 1) * self.entry_size % self.page_size
+
+    def get_column_index(self):
+        return self.column_index
 
     # write to physical page
     def write_to_physical_page(self, value:int, rid:int, update: bool=False)->None:
         # Perform capacity check only if update is False
-        if not update and not self.has_capacity():
+        if not update and not self.__has_capacity():
             raise OverflowError("Not enough space in physical page or Reached record limit")
 
         # Gets offset with RID
-        offset = self.__get_offset(rid) 
+        offset = self.__get_offset(rid)
         start = offset
         # Stop writing at (32 bytes + 8 bytes) = 40 bytes
         end = start + self.entry_size
@@ -40,7 +51,7 @@ class Physical_Page:
         # Convert integer to (entry_size) bytes
         value_bytes = int.to_bytes(value, self.entry_size, byteorder='big', signed=True)
         self.data[start:end] = value_bytes
-        print(f'Inserted value ({value}) in page ({self.column_number}) into Bytes ({start} - {end})')
+        print(f'Inserted value ({value}) in page ({self.column_index}) into Bytes ({start} - {end})')
 
         # Increment num_records only if update is False
         if not update:
@@ -73,17 +84,8 @@ class Physical_Page:
         end = start + self.entry_size
         entry_bytes = self.data[start:end]
         value_in_page = int.from_bytes(entry_bytes, byteorder='big', signed=True)
-        # print(f"Value {value_in_page} was found at Bytes ({start} - {end}) in column {self.column_number}")
         return value_in_page
 
-    # calculates our offset to know where the RID entry is at in the physical page
-    def __get_offset(self, rid:int)->int:
-        if(rid < 0):
-            rid = abs(rid)
-        if self.column_number == 0:
-            return (rid-1) * self.entry_size % INDIRECTION_PAGE_SIZE 
-        return (rid-1) * self.entry_size % PHYSICAL_PAGE_SIZE        
-    
     #read physical page from disk
     def read_from_disk(self, path_to_physical_page: str, column_index: int):
         # Create directory if it doesn't exist
@@ -92,8 +94,8 @@ class Physical_Page:
             os.makedirs(directory)
 
         physical_page_file = open(path_to_physical_page, "rb")
-        physical_page_file.seek(column_index * PHYSICAL_PAGE_SIZE)
-        self.data = physical_page_file.read(PHYSICAL_PAGE_SIZE)
+        physical_page_file.seek(column_index * Config.PHYSICAL_PAGE_SIZE)
+        self.data = physical_page_file.read(Config.PHYSICAL_PAGE_SIZE)
         physical_page_file.close()
 
     #write physical page to disk
@@ -104,9 +106,6 @@ class Physical_Page:
             os.makedirs(directory)
 
         physical_page_file = open(path_to_physical_page, "wb")
-        physical_page_file.seek(column_index * PHYSICAL_PAGE_SIZE)
+        physical_page_file.seek(column_index * Config.PHYSICAL_PAGE_SIZE)
         physical_page_file.write(self.data)
         physical_page_file.close()
-
-        
-    
