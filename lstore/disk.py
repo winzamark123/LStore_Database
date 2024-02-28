d36b7f9758b9926b4e837d2d33b4d6e394ced9d4
Nikko Sanchez <n.sanchez7580@gmail.com>
2024-02-28 02:41:06 -0800

    change .pp to .bin

--- lstore/disk.py
+++ lstore/disk.py
@@ -25,7 +25,7 @@ class Disk:
             return dict(pickle.loads(mdf))
 
     def write_physical_page_to_disk(self, path_to_column_dir:str, physical_page:Physical_Page)->None:
-        with open(os.path.join(path_to_column_dir, f"{physical_page.column_index}.pp"), 'wb') as ppf:
+        with open(os.path.join(path_to_column_dir, f"{physical_page.column_index}.bin"), 'wb') as ppf:
             ppf.write(physical_page.data)
 
     def read_physical_page_from_disk(self, path_to_physical_page:str, is_indirection:bool)->Physical_Page:
@@ -46,15 +46,15 @@ Database Directory
             metadata.pkl -> {page_range_path, num_base_pages}
             BP0 (512 records in base page -> 1 physical page per column)
                 metadata.pkl -> {base_page_path, num_records, page_index. num_tail_pages}
-                0.pp
-                1.pp
+                0.bin
+                1.bin
                 ...
-                8.pp
+                8.bin
             TP0_1
-                0.pp
-                1.pp
+                0.bin
+                1.bin
                 ...
-                8.pp
+                8.bin
             TP0_2
             BP1
             TP1
@@ -62,7 +62,7 @@ Database Directory
     Table 2 Directory (w/ 7 columns)
         PR0
             BP0
-                0.pp
+                0.bin
                 ...
-                10.pp
+                10.bin
 """
